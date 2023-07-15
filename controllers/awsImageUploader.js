14c4d07d7650ed6003a24bb6d14ad3b82d726358
Nesat Nayem <nesatnayem042@gmail.com>
2023-07-15 06:47:52 +0600

    file uploader

--- controllers/awsImageUploader.js
+++ controllers/awsImageUploader.js
@@ -2,7 +2,7 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
 
 const crypto = require("node:crypto")
 
-const randomImageName = (bytes= 32) => crypto.randomBytes(bytes).toString('hex')
+const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
 
 const bucketName = process.env.BUCKET_NAME
 const bucketRegion = process.env.BUCKET_REGION
@@ -10,15 +10,15 @@ const accessKey = process.env.ACCESS_KEY
 const secretAccessKey = process.env.SECRET_KEY
 
 const s3 = new S3Client({
-    credentials:{
-     accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey
- },
- region:bucketRegion
+    credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretAccessKey
+    },
+    region: bucketRegion
 })
 
 
-const S3ImageUploader = async(req,res) =>{
+const S3ImageUploader = async (req, res) => {
 
     console.log("body data", req.body)
     console.log("file data", req.file)
@@ -32,9 +32,16 @@ const S3ImageUploader = async(req,res) =>{
 
     const command = new PutObjectCommand(params)
 
-   await s3.send(command)
+    await s3.send(command)
 
-res.send({imagename:`https://trans-all-files.s3.ap-south-1.amazonaws.com/${imagename}`})
+    res.json({
+        success: true,
+        data: {
+            imageurl: `https://trans-all-files.s3.ap-south-1.amazonaws.com/${imagename}`
+
+        }
+
+    })
 }
 
 module.exports = S3ImageUploader
