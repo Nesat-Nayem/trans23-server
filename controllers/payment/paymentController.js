0a3844b2a8526471c215b8c898b348aa2e45dd9a
Nesat Nayem <nesatnayem042@gmail.com>
2023-07-20 06:21:30 +0600

    payment

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -5,16 +5,16 @@ const crypto = require('crypto');
 const paymentmanage = async (req, res) => {
     const test = req.body.test;
     const data = {
-        // merchantId: process.env.MARCENT_ID,
-        merchantId: test ? 'MERCHANTUAT' : process.env.MARCENT_ID,
+            // merchantId: process.env.MARCENT_ID,
+            merchantId: test ? 'MERCHANTUAT' : process.env.MARCENT_ID,
         merchantTransactionId: req.body.transection_id, // Replace with your own function to generate unique IDs
         merchantUserId: 'jlkjkoeirue',
         amount: req.body.amount * 100,
         redirectUrl: test ? `https://server.trans23.com/api/payment/response?test=${req.body.test}&transactionId=${req.body.transection_id}` : `https://server.trans23.com/api/payment/response?test=${req.body.test}&transactionId=${req.body.transection_id}`,
-        redirectMode: 'GET',
+        redirectMode: 'POST',
         callbackUrl: test ? 'https://server.trans23.com/payment/response'
             : 'https://server.trans23.com/api/payment/response',
-        mobileNumber: '9021557095',
+        mobileNumber: test ? '9021557095' : req.body.phone,
         paymentInstrument: {
             type: 'PAY_PAGE'
         }
@@ -86,15 +86,24 @@ const checkStatus = async (req, res) => {
 
     const merchantId = test ? 'MERCHANTUAT' : process.env.MARCENT_ID;
     // const merchantId = process.env.MARCENT_ID;
+
+
     // const apiUrl = `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${transactionId}`;
+
+    // const apiUrl = `https://mercury-uat.phonepe.com/v3/transaction/${merchantId}/${transactionId}/status`;
+
+
+
     const apiUrl = test ? `https://api-preprod.phonepe.com/apis/merchant-simulator/pg/v1/status/${merchantId}/${transactionId}` : `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${transactionId}`;
+
+
     // const salt_key = process.env.MARCENT_KEY;
     const salt_key = test ? '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399' : process.env.MARCENT_KEY;
     const salt_index = 1;
     const string_to_encode = `/pg/v1/status/${merchantId}/${transactionId}` + salt_key;
     const encoded_string = crypto.createHash('sha256').update(string_to_encode).digest('hex');
     const final_xheader = encoded_string + '###' + salt_index;
-    console.log(final_xheader);
+    console.log(final_xheader, apiUrl);
 
     try {
 
@@ -103,7 +112,7 @@ const checkStatus = async (req, res) => {
             headers: {
                 'Content-Type': 'application/json',
                 'X-VERIFY': final_xheader,
-                'X-MERCHANT-ID': 'MERCHANTUAT'
+                'X-MERCHANT-ID': merchantId
             }
         });
 
@@ -117,7 +126,11 @@ const checkStatus = async (req, res) => {
         res.send(responseData);
     } catch (error) {
         console.error('An error occurred:', error.message);
-        res.status(500).send('Payment failed on response');
+        res.status(500).json({
+            success: false,
+            code: "PAYMENT_ERROR",
+            message: `"Payment Failed", ${error.message} `
+        });
     }
 }
 
