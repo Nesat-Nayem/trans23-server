8d0a18ec488235a6339bf16ea6e231e0001f0520
Nesat Nayem <nesatnayem042@gmail.com>
2023-07-25 07:13:32 +0600

    satelment

--- models/vendor/vendorPaymentModel.js
+++ models/vendor/vendorPaymentModel.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-
-const vendorPaymentSchema = new mongoose.Schema({
-    vendor_id:{
-        type:String
+const vendorPaymentSchema = new mongoose.Schema(
+  {
+    vendor_id: {
+      type: String,
     },
-    week_amount:{
-        type:Number
+    week_amount: {
+      type: Number,
     },
-    status:{
-        type:String
-    }
-  
-},{timestamps:true})
+    status: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const VendorPayment = mongoose.model("VendorPayment",vendorPaymentSchema )
+const VendorPayment = mongoose.model("VendorPayment", vendorPaymentSchema);
 
-module.exports = VendorPayment
+module.exports = VendorPayment;
