f10ce590749f14161ed8214f5590848c03ee005b
Nesat Nayem <nesatnayem042@gmail.com>
2023-11-10 11:31:35 +0600

    otp template changed

--- services/user.services.js
+++ services/user.services.js
@@ -46,13 +46,13 @@ async function createNewOTP(params, callback) {
     const hashs = crypto.createHmac("sha256", key).update(mdata).digest("hex");
     const fullHash = { mdata: `${hashs}.${expires}`, devpass: otp };
 
-    // var msg = urlencode(
-    //   `Hello User,%n %nUse OTP ${otp} to log in to your trans23 account. Do not share your OTP with anyone to keep your account safe.%n %nRegards,%nTrans23 Transportation`
-    // );
-const hash = "12323233456"
     var msg = urlencode(
-      `Trans23 Your Login OTP ${otp} ${hash}`
+      `Hello User,%n %nUse OTP ${otp} to log in to your trans23 account. Do not share your OTP with anyone to keep your account safe.%n %nRegards,%nTrans23 Transportation`
     );
+// const hash = "12323233456"
+//     var msg = urlencode(
+//       `Trans23 Your Login OTP ${otp} ${hash}`
+//     );
 
     var number = `+91${params.phone}`;
     var apikey = "NmI3NTRkNTU3ODM3NWE2ZjRiN2E0YjU0NzIzMDM0NmI=";
